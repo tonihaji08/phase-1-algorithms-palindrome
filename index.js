93e24a2e0e3700cdb7b7e3a88375c294cfdc1c87
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-09 16:14:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  console.log("Expecting:true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+  console.log("Expecting:true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+  console.log("Expecting:true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+  console.log("Expecting:false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+  console.log("Expecting:false");
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("");
+
 }
 
 /* 
@@ -17,9 +38,9 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
-
-  console.log("Expecting: false");
+.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
+  console
 
 module.exports = isPalindrome;
