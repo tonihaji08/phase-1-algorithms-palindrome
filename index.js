1a59b3ccda34eee17b2b70ceb3cdd609b25a2fb2
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-09 19:22:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+function reverseString(word) {
+  {
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+}
+
+
+
+
+
 function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+  
   // Write your algorithm here
-  console.log("Expecting:true");
-  console.log("=>", isPalindrome("abba"));
-
-  console.log("");
-  console.log("Expecting:true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-  console.log("Expecting:true");
-  console.log("=>", isPalindrome("a"));
-
-  console.log("");
-  console.log("Expecting:false");
-  console.log("=>", isPalindrome("robot"));
-
-  console.log("");
-  console.log("Expecting:false");
-  console.log("=>", isPalindrome("ab"));
-
-  console.log("");
-
 }
 
 /* 
@@ -38,9 +41,9 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
-.log("Expecting: false");
+
+  console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-  console
 
 module.exports = isPalindrome;
